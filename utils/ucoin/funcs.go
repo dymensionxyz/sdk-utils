f59310523d0dbc2e6faa3db0fe71429f0667fcf6
danwt <30197399+danwt@users.noreply.github.com>
2024-08-06 14:27:44 +0100

    add ucoin with MulDec and test

---
+++ utils/ucoin/funcs.go
@@ -0,0 +1,13 @@
+package ucoin
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+// MulDec multiplies each coin by dec, truncating down to the nearest integer.
+func MulDec(dec sdk.Dec, coins ...sdk.Coin) sdk.Coins {
+	ret := make(sdk.Coins, len(coins))
+	for i, coin := range coins {
+		ret[i].Denom = coin.Denom
+		ret[i].Amount = dec.MulInt(coin.Amount).TruncateInt()
+	}
+	return ret
+}
