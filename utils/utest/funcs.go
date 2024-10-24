a5e3546982bd2468d2bb53058ebd6a18af1eee3e
danwt <30197399+danwt@users.noreply.github.com>
2024-10-24 18:10:40 +0100

    add to utest

--- utils/utest/funcs.go
+++ utils/utest/funcs.go
@@ -1,1 +1,13 @@
 package utest
+
+import (
+	errorsmod "cosmossdk.io/errors"
+)
+
+type Truer interface {
+	True(value bool, msgAndArgs ...interface{})
+}
+
+func IsErr(t Truer, expected, actual error) {
+	t.True(errorsmod.IsOf(actual, expected))
+}
